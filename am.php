831e7f51b8d8aa72952c8aa84316f08707b453db
Fahad Sheikh <fahaddsheikh@gmail.com>
2019-08-26 22:44:58 +0500

    Added Codin standards

--- am.php
+++ am.php
@@ -16,7 +16,7 @@ class Am {
 	 **/
 	public function __construct() {
 
-		add_action('wp_enqueue_scripts', array( $this, 'enqueue_files' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_files' ) );
 
 		add_action( 'wp_ajax_call', array( $this, 'get_cached_results' ) );
 		add_action( 'wp_ajax_nopriv_call', array( $this, 'get_cached_results' ) );
@@ -25,84 +25,87 @@ class Am {
 	}
 
 	/**
-	 * undocumented function
+	 * Enqueue necessary files
 	 *
 	 * @return void
-	 * @author 
+	 * @author
 	 **/
-	public function enqueue_files()
-	{
-		if ( ! wp_script_is( 'jquery', 'enqueued' )) {
-		    wp_enqueue_script( 'jquery' );
-		} 
+	public function enqueue_files() {
+		if ( ! wp_script_is( 'jquery', 'enqueued' ) ) {
+			wp_enqueue_script( 'jquery' );
+		}
 
-		wp_enqueue_script( 'javascript', plugin_dir_url( __FILE__ ) . 'js/javascript.js', array('jquery'), '0.1', true );
+		wp_enqueue_script( 'javascript', plugin_dir_url( __FILE__ ) . 'js/javascript.js', array( 'jquery' ), '0.1', true );
 
-		wp_localize_script( 'javascript', 'jsobject',
-			array( 
-		    	'ajaxurl' => admin_url( 'admin-ajax.php' )
-		    )
+		wp_localize_script(
+			'javascript',
+			'jsobject',
+			array(
+				'ajaxurl' => admin_url( 'admin-ajax.php' ),
+			)
 		);
 	}
 
 	/**
 	 * Function to call API
 	 *
-	 * @return void
+	 * @return response
 	 * @author Fahad Sheikh
 	 **/
 	public function get_cached_results() {
 		$response = get_transient( 'api_ress' );
 
-		if (false === $response) {
+		if ( false === $response ) {
 			$response = $this->api_call();
 			set_transient( 'api_ress', $response, DAY_IN_SECONDS );
 		}
 
-		return $response;		
+		return $response;
 	}
 
 	/**
-	 * undocumented function
+	 * Function to call the API
 	 *
-	 * @return void
-	 * @author 
+	 * @return response
+	 * @author Fahad Sheikh
 	 **/
-	public function api_call()
-	{
+	public function api_call() {
 		$curl = curl_init();
 
-		curl_setopt_array($curl, array(
-		  CURLOPT_URL => "https://miusage.com/v1/challenge/1/",
-		  CURLOPT_RETURNTRANSFER => true,
-		  CURLOPT_ENCODING => "",
-		  CURLOPT_MAXREDIRS => 10,
-		  CURLOPT_TIMEOUT => 30,
-		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		  CURLOPT_CUSTOMREQUEST => "GET",
-		  CURLOPT_SSL_VERIFYHOST => false,
-		  CURLOPT_SSL_VERIFYPEER => false,
-		  CURLOPT_HTTPHEADER => array(
-		    "Accept: */*",
-		    "Accept-Encoding: gzip, deflate",
-		    "Cache-Control: no-cache",
-		    "Connection: keep-alive",
-		    "Host: miusage.com",
-		    "Postman-Token: f10ee9b8-779c-4f7b-802c-3c5a76c9ea59,0965b535-ab52-4046-aace-fce078c933f9",
-		    "User-Agent: PostmanRuntime/7.15.2",
-		    "cache-control: no-cache"
-		  ),
-		));
+		curl_setopt_array(
+			$curl,
+			array(
+				CURLOPT_URL => 'https://miusage.com/v1/challenge/1/',
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_ENCODING => '',
+				CURLOPT_MAXREDIRS => 10,
+				CURLOPT_TIMEOUT => 30,
+				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+				CURLOPT_CUSTOMREQUEST => 'GET',
+				CURLOPT_SSL_VERIFYHOST => false,
+				CURLOPT_SSL_VERIFYPEER => false,
+				CURLOPT_HTTPHEADER => array(
+					'Accept: */*',
+					'Accept-Encoding: gzip, deflate',
+					'Cache-Control: no-cache',
+					'Connection: keep-alive',
+					'Host: miusage.com',
+					'Postman-Token: f10ee9b8-779c-4f7b-802c-3c5a76c9ea59,0965b535-ab52-4046-aace-fce078c933f9',
+					'User-Agent: PostmanRuntime/7.15.2',
+					'cache-control: no-cache',
+				),
+			)
+		);
 
-		$response = curl_exec($curl);
-		$err = curl_error($curl);
+		$response = curl_exec( $curl );
+		$err = curl_error( $curl );
 
-		curl_close($curl);
+		curl_close( $curl );
 
-		if ($err) {
-		  return "cURL Error #:" . $err;
+		if ( $err ) {
+			return 'cURL Error #:' . $err;
 		} else {
-		  return $response;
+			return $response;
 		}
 	}
 }
